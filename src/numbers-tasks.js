f63479b97cc5ff06f5ab5ed7752f57506d57a8fc
Ueldenarmo <gerygixer@gmail.com>
2025-10-14 15:28:54 +0300

    feat: solve 7 task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -104,8 +104,13 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const dot = x1 * x2 + y1 * y2;
+  const len1 = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const len2 = Math.sqrt(x2 ** 2 + y2 ** 2);
+  const cos = dot / (len1 * len2);
+  const safeCos = Math.min(1, Math.max(-1, cos));
+  return Math.acos(safeCos);
 }
 
 /**
