6066671ba01285df7064222a8111f5675025660b
Ueldenarmo <gerygixer@gmail.com>
2025-10-23 20:41:43 +0300

    solve 31 task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -458,8 +458,9 @@ function isInteger(number) {
  * '4.567abcdefgh' => 4.567
  * 'abcdefgh'      => NaN
  */
-function getFloatOnString(/* str */) {
-  throw new Error('Not implemented');
+function getFloatOnString(str) {
+  const result = Number.parseFloat(str);
+  return Number.isNaN(result) ? NaN : result;
 }
 
 /**
